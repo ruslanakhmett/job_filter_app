97b0386ee5a513f08bf0d58990c8500ff01d9936
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-07-10 20:00:04 +0300

    test ci7

--- job_filter_app/dev_settings.py
+++ job_filter_app/dev_settings.py
@@ -9,12 +9,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.django.db.backends.postgresql",
+#         "NAME": 'django_db',
+#         "USER": 'django_user',
+#         "PASSWORD": '12345wW!',
+#         "HOST": 'localhost',
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": 'django_db',
-        "USER": 'django_user',
-        "PASSWORD": '12345wW!',
-        "HOST": 'localhost',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("POSTGRES_USER", "user"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
