c9f91a8c4a8c3e83d2571942f170d2a90922332d
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-07-11 09:37:28 +0300

    test ci10

--- job_filter_app/settings.py
+++ job_filter_app/settings.py
@@ -1,5 +1,23 @@
 import os
-from .prod_settings import *
+
+
+SECRET_KEY = 'refwekj4kj54kj35nr43fj34f42lf4'
+
+DEBUG = True
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'django_db',
+        "USER": 'django_user',
+        "PASSWORD": '12345wW!',
+        "HOST": 'localhost',
+    }
+}
 
 
 DJANGO_SETTINGS_MODULE="job_filter_app.settings"
